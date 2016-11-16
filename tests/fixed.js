b8ccfd917d6284ed2c3af6930312d7adcc367f3e
Tom Lingham <tom.lingham@iag.com.au>
2016-11-16 11:15:01 +1100

    Adding broken and fixed scenarios

---
+++ tests/fixed.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const assert = require('power-assert');
+
+describe('version', () => {
+  it('should pull the version from the microservices', () => {
+    return Promise.resolve().then(() => {
+      return assert(true === false);
+    });
+  })
+});
+
