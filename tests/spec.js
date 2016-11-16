ef76f3776a48e8144ec8f093dd694d464d016ba6
Tom Lingham <tom.lingham@iag.com.au>
2016-11-16 11:11:14 +1100

    Problem actually appears to be with inlining the return of assert()

--- tests/spec.js
+++ tests/spec.js
@@ -3,7 +3,8 @@
 const assert = require('power-assert');
 
 describe('version', () => {
-  it('should pull the version from the microservices', () => Promise.resolve()
-    .then(() => assert(true === false)));
+  it('should pull the version from the microservices', () => {
+    return Promise.resolve().then(() =>  assert(true === false))
+  })
 });
 
